cdc77eb79ccd628a5dc0a4794a253259ccbfdb34
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-05-08 18:08:27 +0200

    made ceveral code clean-ups and added more counting

--- url_shortener.rb
+++ url_shortener.rb
@@ -18,7 +18,8 @@ class UrlShortener < Sinatra::Base
 
   get '/' do 
     @original_url = params[:original_url] unless params[:original_url].nil?
-    @urls_shortened = REDIS.get("short_url")
+    @urls_shortened = REDIS.get("counter:urls_shortened")
+    @urls_expanded  = REDIS.get("counter:urls_expanded")
     haml :index 
   end
 
@@ -26,88 +27,43 @@ class UrlShortener < Sinatra::Base
     uri = URI::parse(params[:original_url])
     raise "Invalid URL" unless uri.kind_of? URI::HTTP or uri.kind_of? URI::HTTPS
   
-    @urls_shortened = REDIS.incr("short_url")
+    @urls_shortened = REDIS.incr("counter:urls_shortened")
+    @urls_expanded  = REDIS.get("counter:urls_expanded")
     short_url = @urls_shortened.to_s(36)
-      
-    shortner = {
-      :original_url => params[:original_url],
-      :short_url => short_url,
-      :create_date => Time.new.inspect
+    
+    @shortener = {
+      'original_url' => params[:original_url],
+      'short_url' => short_url,
+      'create_date' => Time.new.inspect
     }
   
-    REDIS.set("short_url:#{short_url}", shortner.to_json)
-  
-    @url = shortner
+    save_result = REDIS.set("short_url:#{short_url}", @shortener.to_json)
+    raise "Unable to save the short url" unless save_result == "OK"
+
     haml :index
   end
 
   get '/:short_url' do 
     shortner_json = REDIS.get("short_url:#{params[:short_url]}")
-    
     raise "Url has not been shorted" if shortner_json.nil?
-    
+
+    REDIS.incr("counter:short_url:#{params[:short_url]}")
+    REDIS.incr("counter:urls_expanded")
     redirect JSON.parse(shortner_json)["original_url"]
+  end
+
+  get '/:short_url/inspect' do 
+    shortner_json = REDIS.get("short_url:#{params[:short_url]}")
+    raise "Url has not been shorted" if shortner_json.nil?
+  
+    @shortener         = JSON.parse(shortner_json)
+    @shortened_counter = REDIS.get("counter:short_url:#{params[:short_url]}") || 0
+    @urls_shortened    = REDIS.get("counter:urls_shortened")
+    @urls_expanded     = REDIS.get("counter:urls_expanded")
+    haml :index
   end
 
   error do 
     haml :index 
   end
-
-  def clippy(text, bgcolor='#FFFFFF')
-    html = <<-EOF
-      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-              width="110"
-              height="14"
-              id="clippy" >
-      <param name="movie" value="/flash/clippy.swf"/>
-      <param name="allowScriptAccess" value="always" />
-      <param name="quality" value="high" />
-      <param name="scale" value="noscale" />
-      <param NAME="FlashVars" value="text=#{text}">
-      <param name="bgcolor" value="#{bgcolor}">
-      <embed src="/flash/clippy.swf"
-             width="110"
-             height="14"
-             name="clippy"
-             quality="high"
-             allowScriptAccess="always"
-             type="application/x-shockwave-flash"
-             pluginspage="http://www.macromedia.com/go/getflashplayer"
-             FlashVars="text=#{text}"
-             bgcolor="#{bgcolor}"
-      />
-      </object>
-    EOF
-  end
-  
-  # enable :inline_templates
 end
-
-__END__
-
-@@ layout
-!!! 1.1
-%html
-  %head
-    %title Snip!
-    %link{:rel => 'stylesheet', :href => 'http://www.w3.org/StyleSheets/Core/Modernist', :type => 'text/css'}
-  = yield
-
-@@ index
-%h1.title Snip!
-- unless @url.nil?
-  %code= @url[:original_url]
-  snipped to
-  %a{:href => "#{ENV['HTTP_REFERER']}#{@url[:short_url]}"}
-    = "#{ENV['HTTP_REFERER']}#{@url[:short_url]}"
-#err.warning= env['sinatra.error']
-%form{:method => 'post', :action => '/'}
-  Snip this:
-  %input{:type => 'text', :name => 'original_url', :size => '50'}
-  %input{:type => 'submit', :value => 'snip!'}
-%small copyright &copy;
-%a{:href => 'http://blog.saush.com'}
-  Chang Sau Sheong
-%br
-  %a{:href => 'http://github.com/sausheong/snip'}
-    Full source code
