87aa7b1a2f18d958fbaa448d0e512af5de33a4b8
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-05-13 01:54:51 +0200

    added rounding of the percentages

--- url_shortener.rb
+++ url_shortener.rb
@@ -71,7 +71,7 @@ class UrlShortener < Sinatra::Base
     
     keys = countries.map { |country| "counter:country:short_url:#{params[:short_url]}:#{country}" }
     counters = REDIS.mget(*keys)
-    result = Hash[countries.zip(counters.map { |counter| { 'percentage' => (counter.to_i / (expanded_counter.to_f / 100)), 'value' => counter.to_i } })]
+    result = Hash[countries.zip(counters.map { |counter| { 'percentage' => (counter.to_i / (expanded_counter.to_f / 100).round(2)), 'value' => counter.to_i } })]
     result['unknown'] = result.delete('xx') if result.include?('xx')
     result['unknown'] = { 'percentage' => 0, 'value' => 0 } unless result.include?('xx')
 
@@ -95,7 +95,7 @@ class UrlShortener < Sinatra::Base
   
     keys = referrers.map { |referrer| "counter:referrers:short_url:#{params[:short_url]}:#{referrer}" }
     counters = REDIS.mget(*keys)
-    result = Hash[referrers.zip(counters.map { |counter| { 'percentage' => (counter.to_i / (expanded_counter.to_f / 100)), 'value' => counter.to_i } })]
+    result = Hash[referrers.zip(counters.map { |counter| { 'percentage' => (counter.to_i / (expanded_counter.to_f / 100).round(2)), 'value' => counter.to_i } })]
 
     result['unknown'] = { 'percentage' => 0, 'value' => 0 } unless result.include?('unknown')
     total_percentage = 0
