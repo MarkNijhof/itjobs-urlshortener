8ca24534b8b581849a14f3325a131bc25a46a751
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-05-16 02:39:09 +0200

    still something wrong with the referrer

--- url_shortener.rb
+++ url_shortener.rb
@@ -150,7 +150,8 @@ class UrlShortener < Sinatra::Base
       REDIS.sadd("list:time-in-minutes:short_url:#{short_url}", time_in_minutes)
       REDIS.incr("counter:time-in-minutes:short_url:#{short_url}:#{time_in_minutes}")
 
-      referrer = (request.env['HTTP_REFERER'] || 'unknown').downcase.gsub(/(.*)\/$/, "#{$1}")
+      referrer = (request.env['HTTP_REFERER'] || 'unknown').downcase
+      referrer.gsub!(/(.*)\/$/, "#{$1}")
       REDIS.sadd("list:referrers:short_url:#{short_url}", referrer)
       REDIS.incr("counter:referrers:short_url:#{short_url}:#{referrer}")
 
